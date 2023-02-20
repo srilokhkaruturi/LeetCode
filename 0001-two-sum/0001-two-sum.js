9bfa9c26351f70a471e9ec9ceae9622f9ccd5e38
Srilokh Karuturi <75606167+srilokhkaruturi@users.noreply.github.com>
2023-02-20 16:38:32 -0600

    Time: 95 ms (60.85%), Space: 50.2 MB (5.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // define map/dictionary
+    let myMap = new Map();
+    let outputArray = []
+    
+    nums.forEach((item, index) => {
+        // does target - item exist?
+        if (myMap.has(target - item)){
+            outputArray = [myMap.get(target - item), index]
+            return
+        }else {
+            myMap.set(item,index)
+        }
+    })
+    
+    console.log(myMap)
+    
+    return outputArray
+    
+};
