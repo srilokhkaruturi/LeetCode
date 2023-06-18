26b61ed61f15d34fa353a0ef7da121cd4f7c1a20
Srilokh Karuturi <75606167+srilokhkaruturi@users.noreply.github.com>
2023-06-18 12:11:09 -0500

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # DEFINE HASHMAP via dictionary
-        hashMap = []
+        # define hashset
+        hash_set = []
         
         for i in range(0, len(nums)):
             lookingFor = target - nums[i] 
             if lookingFor in hashMap:
-                return [hashMap.index(lookingFor), i]
+                return [hash_set.index(lookingFor), i]
             else:
-                hashMap.append(nums[i])
+                hash_set.append(nums[i])
             
         return []
