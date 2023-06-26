74dfa4d024ae6812ed61c926c5e660d58164cab8
Srilokh Karuturi <75606167+srilokhkaruturi@users.noreply.github.com>
2023-06-25 21:10:22 -0500

    Time: 174 ms (51.87%), Space: 17 MB (12.46%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -5,9 +5,11 @@ class Solution:
         
         while pointerOne <= pointerTwo:
             if s[pointerOne] != s[pointerTwo]:
+                # case 1 - remove the character from pointerOne will yield palindrome
                 i_one = pointerOne
                 j_one = pointerTwo - 1
                 
+                # case 2 -  remove the character from pointerTwo will yield palindrome
                 i_two = pointerOne + 1
                 j_two = pointerTwo
                 
@@ -15,13 +17,15 @@ class Solution:
                     i_one += 1
                     j_one -= 1
                 
+                # if even, will cross, if off won't cross
                 if(i_one >= j_one):
                     return True
                 
                 while(i_two < j_two and s[i_two] == s[j_two]):
                     i_two += 1
                     j_two -= 1
-                
+                    
+                # if even, will cross, if off won't cross
                 if(i_two >= j_two):
                     return True
                 
