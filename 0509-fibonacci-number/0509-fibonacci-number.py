8f333536cb2a7ed3bee4fa1f10786c82cf466549
Srilokh Karuturi <75606167+srilokhkaruturi@users.noreply.github.com>
2023-07-09 20:40:27 -0500

    Time: 1492 ms (5.09%), Space: 16.4 MB (25.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        mem_array = {}
+        mem_array = {0: 0, 1: 1}
         result = 0
+        
         if n in mem_array:
             return mem_array[n]
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
         else:
             mem_array[n] = self.fib(n-1) + self.fib(n-2)
         
